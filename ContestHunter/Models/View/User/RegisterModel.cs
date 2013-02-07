91c25c9060b172057b0e051ed6c2aa1dc1879562
onetwogoo <onetwogoo@live.com>
2013-02-08 00:07:23 +0800

    整理/User/Register的View，规范Validate显示

---
+++ ContestHunter/Models/View/User/RegisterModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContestHunter.Models.View.User
+{
+    public class RegisterModel
+    {
+        [Required]
+        [Display(Name = "用户名", Description = "您的账户名称，50字以内")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "密码", Description = "找一个安全的密码，并牢记它")]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "电子邮件", Description = "合法的Email地址，它将用于接收验证邮件与密码恢复")]
+        [RegularExpression(@".+@.+\..{2,4}",ErrorMessage="无效的邮件地址")]
+        [MaxLength(100)]
+        public string Email { get; set; }
+    }
+}
