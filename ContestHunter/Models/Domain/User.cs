cd7591cf0297416fc7f29f71b5236246db17b8fc
VaraintF <variantf@gmail.com>
2013-02-07 15:01:25 +0800

    更改文件结构

---
+++ ContestHunter/Models/Domain/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Mail;
+
+namespace ContestHunter.Models.Domain
+{
+    public class User
+    {
+        public static void SendValidationEmail(string name, string password, string email)
+        {
+            MailMessage msg = new System.Net.Mail.MailMessage(); 
+            msg.To.Add(email);
+            msg.From = new MailAddress("Register@ContestHunter.com", "Test", System.Text.Encoding.UTF8); 
+            msg.Subject = "这是测试邮件";//邮件标题 
+            msg.SubjectEncoding = System.Text.Encoding.UTF8;//邮件标题编码 
+            msg.Body = "邮件内容";//邮件内容 
+            msg.BodyEncoding = System.Text.Encoding.UTF8;//邮件内容编码 
+            msg.IsBodyHtml = false;//是否是HTML邮件 
+            msg.Priority = MailPriority.High;//邮件优先级 
+            SmtpClient client = new SmtpClient(); 
+            client.Credentials = new System.Net.NetworkCredential("hellotyvj@gmail.com", "07070078899"); 
+            //上述写你的GMail邮箱和密码 
+            client.Port = 587;//Gmail使用的端口 
+            client.Host = "smtp.gmail.com"; 
+            client.EnableSsl = true;//经过ssl加密 
+            object userState = msg; 
+            client.SendAsync(msg, userState); 
+        }
+    }
+}
