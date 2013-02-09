d237ed38349741bc54ae7656cff55450765021fe
onetwogoo <onetwogoo@live.com>
2013-02-09 18:02:41 +0800

    开始写作Contest/Index

--- ContestHunter/Controllers/ContestController.cs
+++ ContestHunter/Controllers/ContestController.cs
@@ -1,18 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Configuration;
 using System.Web;
 using System.Web.Mvc;
+using ContestHunter.Models.Domain;
 
 namespace ContestHunter.Controllers
 {
     public class ContestController : Controller
     {
-        //
-        // GET: /Contest/
+        static readonly int INDEX_PAGE_SIZE = int.Parse(ConfigurationManager.AppSettings["Contest.Index.PageSize"]);
 
-        public ActionResult Index()
+        [AllowAnonymous]
+        public ActionResult Index(int pagePending = 0, int pageTesting = 0, int pageDone = 0)
         {
+            ViewBag.Pending = Contest.Pending(pagePending * INDEX_PAGE_SIZE, INDEX_PAGE_SIZE);
+            ViewBag.Testing = Contest.Testing(pageTesting * INDEX_PAGE_SIZE, INDEX_PAGE_SIZE);
+            ViewBag.Done = Contest.Done(pageDone * INDEX_PAGE_SIZE, INDEX_PAGE_SIZE);
+            
             return View();
         }
         public ActionResult List()
