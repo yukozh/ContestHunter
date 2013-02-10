19a8a1b8edc79cecdcbc6c81c9a2e52723d7ae91
lydrainbowcat <hyqjlyd@qq.com>
2013-02-10 17:00:32 +0800

    Problem/Show

---
+++ ContestHunter/Controllers/ProblemController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContestHunter.Controllers
+{
+    public class ProblemController : Controller
+    {
+        //
+        // GET: /Problem/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Show()
+        {
+            return View();
+        }
+    }
+}
