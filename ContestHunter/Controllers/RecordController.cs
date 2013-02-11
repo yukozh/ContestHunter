dc7c49511f92156a7d2a2c82e2c5a9ebe8425b34
lydrainbowcat <hyqjlyd@qq.com>
2013-02-11 17:43:02 +0800

    Record页面

---
+++ ContestHunter/Controllers/RecordController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContestHunter.Controllers
+{
+    public class RecordController : Controller
+    {
+        //
+        // GET: /Record/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
