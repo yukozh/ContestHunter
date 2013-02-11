57b3e0991821d4d95b00fb972fe9b9c688fe25e0
lydrainbowcat <hyqjlyd@qq.com>
2013-02-11 15:22:44 +0800

    Problem/Submit

--- ContestHunter/Controllers/ProblemController.cs
+++ ContestHunter/Controllers/ProblemController.cs
@@ -48,5 +48,36 @@ namespace ContestHunter.Controllers
             ViewBag.MaximumMemoryLimit = testCases.Select(t => t.MemoryLimit).DefaultIfEmpty().Max();
             return View(problem);
         }
+        public ActionResult Submit(string id,string contest)
+        {
+            Problem problem;
+            try
+            {
+                problem = Contest.ByName(contest).ProblemByName(id);
+            }
+            catch (ContestNotStartedException)
+            {
+                return RedirectToAction("Error", "Shared", new { msg = "比赛尚未开始，不能提交代码" });
+            }
+            catch (NotAttendedContestException)
+            {
+                return RedirectToAction("Error", "Shared", new { msg = "没有报名比赛，不能提交代码" });
+            }
+            catch (UserNotLoginException)
+            {
+                throw;
+            }
+            catch (ProblemNotFoundException)
+            {
+                return RedirectToAction("Error", "Shared", new { msg = "题目不存在" });
+            }
+            var testCases = from tid in problem.TestCases()
+                            select problem.TestCaseByID(tid);
+
+            ViewBag.TotalTimeLimit = testCases.Select(t => t.TimeLimit).DefaultIfEmpty().Sum();
+            ViewBag.MinimumMemoryLimit = testCases.Select(t => t.MemoryLimit).DefaultIfEmpty().Min();
+            ViewBag.MaximumMemoryLimit = testCases.Select(t => t.MemoryLimit).DefaultIfEmpty().Max();
+            return View(problem);
+        }
     }
 }
