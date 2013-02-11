700d57324e1f97be8a3c424745431eee6a993032
onetwogoo <onetwogoo@live.com>
2013-02-11 13:46:01 +0800

    CKEditor

---
+++ ContestHunter/Controllers/CKEditorController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContestHunter.Controllers
+{
+    public class CKEditorController : Controller
+    {
+        [HttpPost]
+        public ActionResult Upload(string CKEditorFuncNum, HttpPostedFileBase upload)
+        {
+            ViewBag.FuncName = CKEditorFuncNum;
+            byte[] bytes;
+            using (MemoryStream mem = new MemoryStream())
+            {
+                upload.InputStream.CopyTo(mem);
+                bytes = mem.ToArray();
+            }
+            ViewBag.URI = string.Format("data:{0};base64,{1}", upload.ContentType, Convert.ToBase64String(bytes));           
+            return View();
+        }
+
+    }
+}
