97fc54e357964fdac79cb9eb51d35becdd4abcf1
onetwogoo <onetwogoo@live.com>
2013-02-12 20:53:35 +0800

    Problem/Submit

---
+++ ContestHunter/Models/View/ProblemSubmitModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using ContestHunter.Models.Domain;
+namespace ContestHunter.Models.View
+{
+    public class ProblemSubmitModel
+    {
+        [Required]
+        public string Code { get; set; }
+        [Required]
+        public Record.LanguageType? Language { get; set; }
+
+        public string Problem { get; set; }
+        public string Contest { get; set; }
+    }
+}
