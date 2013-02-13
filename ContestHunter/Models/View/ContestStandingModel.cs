f36129811ecb68bc96214cfbd393663a466b6a7b
onetwogoo <onetwogoo@live.com>
2013-02-13 16:46:32 +0800

    ACMStanding

---
+++ ContestHunter/Models/View/ContestStandingModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ContestHunter.Models.Domain;
+namespace ContestHunter.Models.View
+{
+    public class ContestStandingModel
+    {
+        public string Contest { get; set; }
+        public List<string> Problems { get; set; }
+        public Contest.ContestType Type { get; set; }
+
+        public int PageCount { get; set; }
+        public int PageIndex { get; set; }
+        public int StartIndex { get; set; }
+
+        public List<ACMStanding> ACM { get; set; }
+        public List<CFStanding> CF { get; set; }
+    }
+}
