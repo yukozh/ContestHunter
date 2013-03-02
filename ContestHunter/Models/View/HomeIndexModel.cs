14ee53b8cfb88518e5e1e451f299de7e7fc598c8
onetwogoo <onetwogoo@live.com>
2013-03-02 10:28:22 +0800

    Home/Index

---
+++ ContestHunter/Models/View/HomeIndexModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using ContestHunter.Models.Domain;
+
+namespace ContestHunter.Models.View
+{
+    public class HomeIndexModel
+    {
+        public List<User> Rating { get; set; }
+    }
+}
