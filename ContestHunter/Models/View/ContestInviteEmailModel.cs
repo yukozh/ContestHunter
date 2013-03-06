8c00987be51efe5048dbf1a123b72d69379d1df8
onetwogoo <onetwogoo@live.com>
2013-03-06 20:24:59 +0800

    InviteEmail

---
+++ ContestHunter/Models/View/ContestInviteEmailModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace ContestHunter.Models.View
+{
+    public class ContestInviteEmailModel
+    {
+        public string Contest { get; set; }
+        [DisplayFormat(ConvertEmptyStringToNull=false)]
+        public string Content { get; set; }
+    }
+}
