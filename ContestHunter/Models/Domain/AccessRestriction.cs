5ae7d2726fc07ec804f71a8aa0bd8c60ac20a5da
VaraintF <sunjiayu_2006@gmail.com>
2013-03-15 11:13:31 +0800

    修改访问限制算法

--- ContestHunter/Models/Domain/AccessRestriction.cs
+++ ContestHunter/Models/Domain/AccessRestriction.cs
@@ -2,32 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Collections.Concurrent;
+using System.Threading;
 
 namespace ContestHunter.Models.Domain
 {
     public class AccessRestriction
     {
-        static Dictionary<string, Queue<DateTime>> accessTable = new Dictionary<string, Queue<DateTime>>();
-
+        class Accession
+        {
+            public int Pressure;
+            public DateTime LastAccessTime;
+        }
+        static ConcurrentDictionary<string, Accession> accessTable = new ConcurrentDictionary<string, Accession>();
         public static void CheckRestriction(string ip)
         {
-            string key=ip;
+            string key = ip;
             if (null != User.CurrentUser)
                 key = User.CurrentUserName;
-            lock (accessTable)
+            Accession ac = accessTable.GetOrAdd(key, new Accession()
             {
-                if (!accessTable.ContainsKey(key))
-                    accessTable.Add(key, new Queue<DateTime>());
-                var queue = accessTable[key];
-                if (queue.Count > 0 && DateTime.Now - queue.Last() < TimeSpan.FromSeconds(1))
-                    throw new AccessTooFrequentlyException();
-                var poptime = DateTime.Now - TimeSpan.FromMinutes(5);
-                while (queue.Count > 0 && queue.First() < poptime)
-                    queue.Dequeue();
-                if (queue.Count > 100)
-                    throw new AccessTooFrequentlyException();
-                queue.Enqueue(DateTime.Now);
+                Pressure = 0,
+                LastAccessTime = DateTime.Now
+            });
+            lock (ac)
+            {
+                var span = DateTime.Now - ac.LastAccessTime;
+                if (span.TotalSeconds <= 3)
+                    ac.Pressure++;
+                else if (span.TotalSeconds >= 30)
+                    ac.Pressure -= (int)span.TotalSeconds / 30;
+                Math.Max(0, ac.Pressure);
+                ac.LastAccessTime = DateTime.Now;
             }
+            if (ac.Pressure > 10)
+                throw new AccessTooFrequentlyException();
         }
     }
 }
