6130829675405ce3b2546ec9e6ae389937dc6dd0
onetwogoo <onetwogoo@live.com>
2013-03-26 15:21:39 +0800

    2*Controllers

---
+++ ContestHunter/Controllers/ChatApiController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ContestHunter.Models.Domain;
+namespace ContestHunter.Controllers
+{
+    public class ChatApiController : ApiController
+    {
+        public IEnumerable<Chat.Message> GetCommon(DateTime before, int top)
+        {
+            return Chat.GetCommon(before, top);
+        }
+    }
+}
