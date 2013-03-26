c3b06c40545c8f0200f3ed5ac910f1e7e5c8e171
lydrainbowcat <hyqjlyd@qq.com>
2013-03-26 17:41:20 +0800

    公聊页面

---
+++ ContestHunter/Controllers/ChatController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContestHunter.Controllers
+{
+    public class ChatController : Controller
+    {
+        //
+        // GET: /Chat/
+
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous] //便于调试
+        public ActionResult Public()
+        {
+            return View();
+        }
+    }
+}
